654fa4459f1ef03a21102672a5e947cefc7a3cbf
bhyerain <seunghye0307@naver.com>
2024-08-04 02:27:35 +0900

    chore(product_page): 상품 비교 페이지 구성 변경 및 페이지 link 추가

--- frontend/src/product_page/components/ProductCard.jsx
+++ frontend/src/product_page/components/ProductCard.jsx
@@ -1,8 +1,14 @@
 const ProductCard = ({ product }) => {
   return (
     <div className="product-card">
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
+      <p className="product-name">{product.name}</p>
+      <p className="product-description">{product.description}</p>
+      <button onClick={() => (window.location.href = product.link)}>
+        바로가기
+      </button>
+      <div className="separator"></div>
+      <p className="product-name">{product.name}</p>
+      <p className="product-description">{product.description}</p>
       <button onClick={() => (window.location.href = product.link)}>
         바로가기
       </button>
