2e8057df3ebca568cde06e0debcf8a86783a931b
minnje0ng <laputais34@gmail.com>
2024-08-04 22:27:26 +0900

    feat: signup 버튼 클릭 시 페이지 이동

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProductPage from "./product_page/product_page";
-import Login from "./feat/Login";
-import SignUp from "./feat/SignUp";
-import DepositPage from "./product_page/DepositPage";
-import SavingPage from "./product_page/SavingPage";
-import LoanPage from "./product_page/LoanPage";
-import CheckCardPage from "./product_page/CheckCardPage";
+import Login from "./feat/Login/Login";
+import SignUp from "./feat/SignUp/SignUp";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProductPage />} />
-        <Route path="/" element={<DepositPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/deposit" element={<DepositPage />} />
-        <Route path="/saving" element={<SavingPage />} />
-        <Route path="/loan" element={<LoanPage />} />
-        <Route path="/checkcard" element={<CheckCardPage />} />
       </Routes>
     </BrowserRouter>
   );
 };
+
 export default App;
