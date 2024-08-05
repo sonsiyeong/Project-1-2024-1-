34317d190eb2198c108361e0724b7da39250086b
bhyerain <seunghye0307@naver.com>
2024-08-05 16:30:57 +0900

    chore(ProductPage): product_page 폴더명 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login&SignUp/Login";
 import SignUp from "./Login&SignUp/SignUp";
-import DepositPage from "./product_page/DepositPage";
-import SavingPage from "./product_page/SavingPage";
-import LoanPage from "./product_page/LoanPage";
-import CheckCardPage from "./product_page/CheckCardPage";
+import DepositPage from "./ProductPage/DepositPage";
+import SavingPage from "./ProductPage/SavingPage";
+import LoanPage from "./ProductPage/LoanPage";
+import CheckCardPage from "./ProductPage/CheckCardPage";
 
 const App = () => {
   return (
