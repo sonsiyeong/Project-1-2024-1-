5a640411e0409cf5d05008c9a81737938b234258
bhyerain <seunghye0307@naver.com>
2024-08-08 15:02:56 +0900

    chore(DetailedPage): 코드 수정

--- frontend/src/pages/ProductPage/ProductPage.styles.js
+++ frontend/src/pages/ProductPage/ProductPage.styles.js
@@ -84,11 +84,74 @@ export const SearchIcon = styled.div`
   color: #186915;
 `;
 
-export const AppMain = styled.main`
+export const BankLogo = styled.img`
+   {
+    width: 100px;
+    height: 20px;
+    display: block;
+    margin: 0 auto 20px;
+  }
+`;
+export const DetailContainer = styled.div`
+  padding: 5px;
+`;
+
+export const DetailTitle = styled.h2`
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const DetailSection = styled.section`
+  margin-bottom: 40px;
+`;
+
+export const DetailImage = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: #d3d3d3;
   display: flex;
-  justify-content: space-around;
-  padding: 20px;
-  gap: 5px;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const DetailDescription = styled.p`
+  font-size: 18px;
+  margin-bottom: 20px;
+`;
+
+export const CommentSection = styled.div`
+  margin-top: 40px;
+`;
+
+export const CommentInput = styled.textarea`
+  width: 100%;
+  height: 100px;
+  padding: 10px;
+  border: 1px solid #d9d9d9;
+  border-radius: 5px;
+  margin-bottom: 10px;
+`;
+
+export const CommentButton = styled.button`
+  background-color: #186915;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const BackButton = styled(Link)`
+  display: inline-block;
+  background-color: #186915;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  text-decoration: none;
+  text-align: center;
+  margin-top: 20px;
 `;
 
 export const BankSectionContainer = styled.div`
