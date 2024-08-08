cad4fec04e9f696d537025b7a05f2647da281b46
bhyerain <seunghye0307@naver.com>
2024-08-08 16:52:10 +0900

    feat(ProductPage): 스크랩 기능 추가

--- frontend/src/pages/ProductPage/components/ProductCard.jsx
+++ frontend/src/pages/ProductPage/components/ProductCard.jsx
@@ -1,21 +1,63 @@
+import { useState } from "react";
 import {
   ProductCardWrapper,
   ProductName,
   ProductDescription,
   ProductButton,
   Separator,
+  BookmarkIcon,
+  Popup,
+  ConfirmButton,
 } from "../styles";
+import { FaRegBookmark, FaBookmark } from "react-icons/fa";
 
 const ProductCard = ({ product }) => {
+  const { name } = product;
+  const [bookmarked, setBookmarked] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupMessage, setPopupMessage] = useState("");
+
+  const handleBookmarkClick = () => {
+    if (bookmarked) {
+      setPopupMessage("MY 스크랩에서 삭제되었습니다.");
+    } else {
+      setPopupMessage("MY 스크랩에 저장되었습니다.");
+    }
+    setBookmarked(!bookmarked);
+    setShowPopup(true);
+  };
+
+  const handleConfirmClick = () => {
+    setShowPopup(false);
+  };
   return (
     <ProductCardWrapper>
-      <ProductName>{product.name}</ProductName>
+      {showPopup && (
+        <Popup>
+          <p>{popupMessage}</p>
+          <ConfirmButton onClick={handleConfirmClick}>확인</ConfirmButton>
+        </Popup>
+      )}
+
+      <ProductName>
+        {" "}
+        <BookmarkIcon onClick={handleBookmarkClick}>
+          {bookmarked ? <FaBookmark /> : <FaRegBookmark />}
+        </BookmarkIcon>
+        {name}
+      </ProductName>
       <ProductDescription>{product.description}</ProductDescription>
-      <ProductButton to="/detailedpage">자세히 보기</ProductButton>
+      <ProductButton to={product.link}>자세히 보기</ProductButton>
       <Separator />
-      <ProductName>{product.name}</ProductName>
+      <ProductName>
+        {" "}
+        <BookmarkIcon onClick={handleBookmarkClick}>
+          {bookmarked ? <FaBookmark /> : <FaRegBookmark />}
+        </BookmarkIcon>
+        {name}
+      </ProductName>
       <ProductDescription>{product.description}</ProductDescription>
-      <ProductButton to="/detailedpage">자세히 보기</ProductButton>
+      <ProductButton to={product.link}>자세히 보기</ProductButton>
     </ProductCardWrapper>
   );
 };
