13842f27f81fc18e677409cf63367073950456ab
syyeong <sonssan0917@gmail.com>
2024-08-08 15:05:08 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
 // import 경로 최적화하기
 // import { Login , SignUp, DepositPage, SavingPage, LoanPage, CheckCardPage } from "./pages";
-import Login from "./pages/Login/Login";
-import SignUp from "./pages/SignUp/SignUp";
-import DepositPage from "./pages/ProductPage/DepositPage";
-import SavingPage from "./pages/ProductPage/SavingPage";
-import LoanPage from "./pages/ProductPage/LoanPage";
-import { CheckCardPage } from "./pages/ProductPage";
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import DepositPage from './pages/ProductPage/DepositPage';
+import SavingPage from './pages/ProductPage/SavingPage';
+import LoanPage from './pages/ProductPage/LoanPage';
+import { CheckCardPage } from './pages/ProductPage';
 // TODO: 글로벌 스타일 전역으로 옮기기
-import { GlobalStyle } from "./pages/ProductPage/styles";
+import { GlobalStyle } from './pages/ProductPage/styles';
 //import DetailedPage from "./pages/DetailedPage/DetailedPage"; <Route path="/detailedpage" element={<DetailedPage />} />
 
 const App = () => {
@@ -16,7 +17,8 @@ const App = () => {
     <Router>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<DepositPage />} />
+        <Route path="/mainpage" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/deposit" element={<DepositPage />} />
