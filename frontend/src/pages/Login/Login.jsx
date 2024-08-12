659d4c1454c9bbcd4f423fa2d80932bb0cf8b287
minnje0ng <laputais34@gmail.com>
2024-08-12 22:55:45 +0900

    refactor(Login): react-hook-form과 yup을 이용해 유효성 검사 폼 수정

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigate } from "react-router-dom";
 import * as s from "./Login.styles";
+import LoginValidationSchema from "./validation/LoginValidationSchema";
 
 const Login = () => {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(LoginValidationSchema),
+  });
+
   const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+  const onSubmit = (data) => {
     //백엔드에서 API 제공 시 수정할 부분;
-    if (id === "testuser" && password === "password123") {
+    if (data.id === "testuser" && data.password === "password123") {
       alert("로그인 성공");
     } else {
       setShowError(true);
@@ -33,24 +41,18 @@ const Login = () => {
       </s.Header>
       <s.LoginBar>LOGIN</s.LoginBar>
       <s.LoginContainer>
-        <s.LoginForm onSubmit={handleLogin}>
+        <s.LoginForm onSubmit={handleSubmit(onSubmit)}>
           <s.LoginFormGroup>
             <s.Label>ID</s.Label>
-            <s.Input
-              type="text"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-              required
-            />
+            <s.Input type="text" {...register("id")} />
+            {errors.id && <s.ErrorMessage>{errors.id.message}</s.ErrorMessage>}
           </s.LoginFormGroup>
           <s.LoginFormGroup>
             <s.Label>PASSWORD</s.Label>
-            <s.Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+            <s.Input type="password" {...register("password")} />
+            {errors.password && (
+              <s.ErrorMessage>{errors.password.message}</s.ErrorMessage>
+            )}
           </s.LoginFormGroup>
           <s.LoginButtonGroup>
             <s.LoginButton type="submit">LOGIN</s.LoginButton>
