c1a0832ca3d108ebaecac191d5870176685cfa62
bhyerain <seunghye0307@naver.com>
2024-08-15 04:26:01 +0900

    chore: 코드 오류 수정

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigate } from "react-router-dom";
 import * as S from "../styles/Login.styles";
-import LoginValidationSchema from "../pages/validation/LoginValidationSchema";
+import LoginValidationSchema from "../pages/SignUp/validation/SignUpValidationSchema.js";
 
 export const Login = () => {
   const {
