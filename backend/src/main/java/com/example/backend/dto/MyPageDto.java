88276ffaaf34e432a6347e0c6f6f061a5a9b0615
cherryan613 <cherry613@ewhain.net>
2024-08-15 22:20:18 +0900

    feat(MyPage): MyPage 기능 추가

---
+++ backend/src/main/java/com/example/backend/dto/MyPageDto.java
@@ -0,0 +1,32 @@
+package com.example.backend.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor // 모든 필드를 매개변수로 갖는 생성자 자동 생성
+@NoArgsConstructor // 매개변수가 아예 없는 기본 생성자 자동 생성
+@Getter // 각 필드 값을 조회할 수 있는 getter 메서드 자동 생성
+@Setter // 각 필드 값을 설정할 수 있는 setter 메서드 자동 생성
+@ToString // 모든 필드를 출력할 수 있는 toString 메서드 자동 생성
+
+public class MyPageDto {
+    private String userName;
+    private String userEmail;
+    private String userId;
+    private List<ReviewDto> reviewDtoList;
+
+    public MyPageDto(String userName, String userEmail, String userId){
+        this.userName=userName;
+        this.userEmail=userEmail;
+        this.userId=userId;
+    }
+
+    public static MyPageDto createMyPageDto(MyPageDto myPageDto){
+        return new MyPageDto(
+          myPageDto.userName,
+          myPageDto.userEmail,
+          myPageDto.userId
+        );
+    }
+}
