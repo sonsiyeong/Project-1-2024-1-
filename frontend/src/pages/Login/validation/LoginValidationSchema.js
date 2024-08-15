c3d0b187dfbd11c311cd793943e35dc0ba734a5f
minnje0ng <laputais34@gmail.com>
2024-08-15 15:05:48 +0900

    chore(Login, SignUp): 폴더 삭제

--- frontend/src/pages/Login/validation/LoginValidationSchema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-const LoginValidationSchema = yup.object().shape({
-  id: yup.string().required("ID를 입력해주세요."),
-  password: yup.string().required("비밀번호를 입력해주세요."),
-});
-
-export default LoginValidationSchema;
