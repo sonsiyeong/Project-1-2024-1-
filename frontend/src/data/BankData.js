1105bfc33e88df1402d0e0d15e6fe62ad82cc489
bhyerain <seunghye0307@naver.com>
2024-08-15 19:58:21 +0900

    chore(product): bankdata 따로 처리

---
+++ frontend/src/data/BankData.js
@@ -0,0 +1,37 @@
+export const bankData = [
+  {
+    name: "KB국민은행",
+    products: [
+      { id: 1, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
+    ],
+    logoKey: "kb",
+  },
+  {
+    name: "NH농협은행",
+    products: [
+      { id: 2, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
+    ],
+    logoKey: "nh",
+  },
+  {
+    name: "신한은행",
+    products: [
+      { id: 3, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
+    ],
+    logoKey: "sh",
+  },
+  {
+    name: "우리은행",
+    products: [
+      { id: 4, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
+    ],
+    logoKey: "woori",
+  },
+  {
+    name: "하나은행",
+    products: [
+      { id: 5, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
+    ],
+    logoKey: "hana",
+  },
+];
--- frontend/src/pages/ProductPage/Deposit.jsx
+++ frontend/src/pages/ProductPage/Deposit.jsx
@@ -1,44 +1,7 @@
 import { Header } from "../../components/index.js";
 import * as S from "../../styles/Product.styles.js";
 import BankSection from "../../components/BankSection.jsx";
-
-const bankData = [
-  {
-    name: "KB국민은행",
-    products: [
-      { id: 1, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
-    ],
-    logoKey: "kb",
-  },
-  {
-    name: "NH농협은행",
-    products: [
-      { id: 2, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
-    ],
-    logoKey: "nh",
-  },
-  {
-    name: "신한은행",
-    products: [
-      { id: 3, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
-    ],
-    logoKey: "sh",
-  },
-  {
-    name: "우리은행",
-    products: [
-      { id: 4, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
-    ],
-    logoKey: "woori",
-  },
-  {
-    name: "하나은행",
-    products: [
-      { id: 5, name: "상품명", link: "#", description: "특징 한 눈에 보기" },
-    ],
-    logoKey: "hana",
-  },
-];
+import { bankData } from "../../data/BankData.js";
 
 export const Deposit = () => {
   return (
