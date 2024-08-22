efb36b4f14e57c7d9abaf8a83ac5ebeaeb185eb3
minnje0ng <laputais34@gmail.com>
2024-08-22 23:53:33 +0900

    chore: 헤더 styled-component 파일 이름 변경

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import * as S from "./header.style.js";
+import * as S from "./header.styles.js";
 
 export function Header() {
   const [bankName, setBankName] = useState("");
