f6c65436cae47972728c66a614afe4cb2974a2b2
syyeong <sonssan0917@gmail.com>
2024-08-23 13:21:40 +0900

    chore(Login.jsx):유저코드저장

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -43,6 +43,7 @@ export const Login = () => {
         if (result.message === "Success") {
           window.sessionStorage.setItem("token", result.token);
           window.sessionStorage.setItem("role", result.role);
+          window.sessionStorage.setItem("userId", result.userId);
           window.sessionStorage.setItem("userCode", result.userCode); // userCode 저장
           setIsLogin(true);  // 로그인 상태로 설정
 
@@ -116,3 +117,4 @@ export const Login = () => {
 };
 
 export default Login;
+
